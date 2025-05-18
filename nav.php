0b8e058e627bc2043aa94eedb7fde8889a91bc07
alexb-tripshock <75819310+alexb-tripshock@users.noreply.github.com>
2025-05-17 22:02:01 -0400

    first commit

---
+++ nav.php
@@ -0,0 +1,22 @@
+< !doctype html>
+
+<ul>
+											<li><a href="/">Home</a></li>
+											<li><a href="about-crab-island.html">About</a></li>
+
+<li><a href="how-to-get-to-crab-island.html">How To Get There</a></li>
+
+<li><a href="crab-island-pictures.html">Pictures</a></li>
+
+<li><a href="hotels-near-crab-island.html">Hotels Nearby</a></li>
+
+<li><a href="crab-island-boat-rentals.html">Boat Rentals & Tours</a></li>
+
+<li><a href="crab-island-activities.html">Other Activities</a></li>
+											
+<!--
+<li><a href="elements.html">Elements</a></li>
+											<li><a href="#">Sign Up</a></li>
+											<li><a href="#">Log In</a></li>
+-->
+										</ul>
